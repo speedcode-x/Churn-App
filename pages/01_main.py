ecbdd56457d2feec48f4b70e5cceb0b188fef93c
speedStar <philipkwaning.o@gmail.com>
2024-02-23 16:54:04 +0100

    add virtual env and pages

---
+++ pages/01_main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+
+st.set_page_config(
+    page_icon='',
+    page_title="Home",
+    layout='wide'
+)
+
+st.title('Hello World')
+st.button('Submit')
+st.text_input('Enter your text')
